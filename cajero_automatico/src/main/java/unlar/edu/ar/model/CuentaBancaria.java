00dc5edefcf07f17ea1b529a05727f74bfbfb61b
Camila Lucero <camilalucero076@gmail.com>
2026-04-26 02:52:42 -0300

    Getters y Setters de CuentaBancaria

--- cajero_automatico/src/main/java/unlar/edu/ar/model/CuentaBancaria.java
+++ cajero_automatico/src/main/java/unlar/edu/ar/model/CuentaBancaria.java
@@ -1,5 +1,67 @@
 package unlar.edu.ar.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.ArrayList;
+
 public class CuentaBancaria {
 
+    private final String numeroCuenta;  // final porque debe ser inmutable
+    private BigDecimal saldo;
+    private String titular;
+    private boolean activa;
+    private List<String> historialTransacciones;
+
+    public CuentaBancaria(String numeroCuenta, BigDecimal saldoInicial, String titular, boolean activa,
+            List<String> historialTransacciones) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+        this.titular = titular;
+        this.activa = true;
+        this.historialTransacciones = new ArrayList<>();
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+
+    public List<String> getHistorialTransacciones() {
+        return new ArrayList<>(historialTransacciones);
+    }
+
+    
+    public void setHistorialTransacciones(List<String> historialTransacciones) {
+        if (this.historialTransacciones.size() >= 10) {
+
+            this.historialTransacciones.remove(0);   // elimina la mas antigua
+
+        }  
+        this.historialTransacciones.add(log);
+    }
+
+    
 }
